8273ad7792830100da73c4e2d79d757a4ff9fe14
MuhaddisRehman <96332700+MuhaddisRehman@users.noreply.github.com>
2023-07-02 23:09:23 +0500

    rooms components

---
+++ src/app/components/rooms/rooms.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Room } from './rooms';
+
+@Component({
+  selector: 'app-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.scss']
+})
+export class RoomsComponent {
+  hotelName:string = 'Mazay'
+  numberOfRooms:number = 10;
+  hideRooms:boolean = false;
+
+  rooms:Room={
+    totalRooms:20,
+    availableRooms:10,
+    bookedRooms:5
+  }
+}
